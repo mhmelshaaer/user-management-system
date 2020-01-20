32d8a37f0a7adfc2bb8e106d95b5227431808067
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2020-01-20 21:05:25 +0200

    daily registration chart

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class PagesController extends Controller
 {
     public function dashboard(Request $request)
     {
+
         if(!auth()->user()->hasPermissionTo('admin'))
         {
             if (!auth()->user()->hasPaymentMethod())
@@ -18,9 +21,21 @@ class PagesController extends Controller
             }
         }
 
-        return view('home', [
+        $data = [
             'user' => auth()->user()
-        ]);
+        ];
+
+        if (auth()->user()->hasPermissionTo('admin'))
+        {
+
+            $data['users'] = [
+                'units' => User::whereDay('created_at', now()->day)->count(),
+                'date' => now()
+            ];
+
+        }
+
+        return view('home', $data);
 
     }
 
@@ -28,4 +43,5 @@ class PagesController extends Controller
     {
         return view('users');
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,72 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">Hello MOFO, You are logged in as {{$user->name}}!</p>
+
+                    @can('admin')
+
+                        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+                    @else
+
+                        <p class="mb-0">Hello MOFO, You are logged in as {{$user->name}}!</p>
+
+                    @endcan
+
                 </div>
             </div>
         </div>
     </div>
 @stop
+
+@section('css')
+
+@stop
+
+@can('admin')
+
+    @section('js')
+
+        <script>
+
+            window.onload = function() {
+
+                var dataPoints = [];
+                var todayUsers = @json($users);
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    theme: "light2",
+                    title: {
+                        text: "Daily Registration"
+                    },
+                    axisY: {
+                        title: "Users",
+                        titleFontSize: 24
+                    },
+                    data: [{
+                        type: "column",
+                        yValueFormatString: "#,### User",
+                        dataPoints: dataPoints
+                    }]
+                });
+
+                function addData(data) {
+
+                    dataPoints.push({
+                        x: new Date(data.date),
+                        y: data.units
+                    });
+                    chart.render();
+
+                }
+
+                addData(todayUsers);
+
+            }
+        </script>
+
+        <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+        <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    @stop
+
+@endcan
